63e07a44efc3a36ada62f4dd4a5bfa5df606f205
RiouC <riou.christophe@protonmail.com>
2021-06-01 03:18:54 +0200

    [GlobalSettings] localstorage getItem

--- src/context/GlobalSettingsContext.js
+++ src/context/GlobalSettingsContext.js
@@ -3,8 +3,8 @@ import { createContext, useState } from "react";
 export const GlobalSettingsContext = createContext();
 
 export const GlobalSettingsContextProvider = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(true);
-  const [lang, setLang] = useState("en");
+  const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem('dark-mode')) || true);
+  const [lang, setLang] = useState(JSON.parse(localStorage.getItem('lang')) || "en");
   const langMessages = {
     "en": {"tagline": "Ultimate collection of the most beautiful shades",
            "add": "Add",
